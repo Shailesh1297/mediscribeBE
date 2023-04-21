00e2e80e8cf06444db36e7e8ab40f2692bb644ec
Shailesh Rai <shaileshrai1001@gmail.com>
2023-04-21 20:37:35 +0530

    [4348531084]Create Boilerplate code for BE

---
+++ src/main/java/org/mediscribe/MediscribeApplication.java
@@ -0,0 +1,13 @@
+package org.mediscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediscribeApplication.class, args);
+    }
+
+}
